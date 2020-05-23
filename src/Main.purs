306178dc39cbf0d4890488577a8e414f6f946ea2
David Petersen <david@familiep.de>
2020-05-23 13:03:59 +0200

    ajax calls and json decoding

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,17 @@ module Main (main) where
 
 import Prelude
 
+import Affjax as AX
+import Affjax.ResponseFormat as RF
+import Data.Argonaut as A
+import Data.Either (Either(..))
+import Data.Foldable (for_)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String as String
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Effect.Class.Console as Console
+import Halogen (liftAff)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
@@ -13,10 +21,11 @@ import Halogen.HTML.Properties as HP
 import Halogen.VDom.Driver (runUI)
 
 type State = { label :: Maybe String, currentInput :: String }
+type Repo = { name :: String }
 
 data Action = Toggle | InputChanged String
 
-component :: forall q i o m. H.Component HH.HTML q i o m
+component :: forall q i o. H.Component HH.HTML q i o Aff
 component =
   H.mkComponent
     { initialState
@@ -46,13 +55,27 @@ render state =
       ]
     
 
-handleAction ∷ forall o m. Action → H.HalogenM State Action () o m Unit
+handleAction ∷ forall o. Action → H.HalogenM State Action () o Aff Unit
 handleAction = case _ of
-  Toggle -> H.modify_ \st -> st { label = Just st.currentInput }
-  InputChanged newText -> H.modify_ (_ { currentInput = newText }) -- \st -> st <-> (_ {...})
+  Toggle -> do
+    liftAff (AX.get RF.json "https://api.github.com/users/alexdobry/repos") >>= case _ of
+      Left err -> 
+        Console.log (AX.printError err)
+      Right {body} -> case A.decodeJson body of
+        Left parseErr -> 
+          Console.log parseErr
+        Right (repos :: Array Repo) -> 
+          for_ repos (Console.log <<< _.name)
+            
+
+    H.modify_ \st -> st { label = Just st.currentInput }
+
+
+  InputChanged newText ->
+     Console.log newText >>= \_ ->
+       H.modify_ (_ { currentInput = newText }) -- \st -> st <-> (_ {...})
     --H.modify_ \st -> st { enabled = not st.enabled }
 
 main :: Effect Unit
-main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI component unit body
+main = HA.runHalogenAff
+  (HA.awaitBody >>= runUI component unit)
